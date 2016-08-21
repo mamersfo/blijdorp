2a0746e5ea5d2f901471e82aac507e843eb75399
Martin van Amersfoorth <mamersfo@gmail.com>
2016-08-21 10:31:29 +0200

    Added t param to Youtube URL

--- app/exercises.js
+++ app/exercises.js
@@ -59,7 +59,7 @@ export default class Exercises extends React.Component {
     if ( undefined === m.v ) {
       return <span>{m.n}</span>
     } else {
-      let url = 'https://www.youtube.com/watch?v=' + m.v
+      let url = 'https://www.youtube.com/watch?v=' + m.v + '&t=' + ( undefined !== m.t ? m.t : 0 )
       let img = 'images/' + m.s + '.png'
       return (
         <a href={url} target='_blank'>
