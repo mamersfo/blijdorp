95fe3958ae9e71dc0bb171c96cad413f60c25101
Martin van Amersfoorth <mamersfo@gmail.com>
2017-09-20 09:02:57 +0200

    Use 24 hour clock

--- app/datetime.js
+++ app/datetime.js
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 export class LocalDate extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       locale: props.locale || 'NL-nl',
       day: props.day || 'numeric',
       month: props.month || 'long'
-    }
+    };
   }
   
   render() {
@@ -15,31 +15,31 @@ export class LocalDate extends React.Component {
       this.props.date.toLocaleDateString(
         this.state.locale,
         {day: this.state.day, month: this.state.month}
-      ) : ''
+      ) : '';
 
-    return <span>{s}</span>
+    return <span>{s}</span>;
   }
 }
 
 export class LocalTime extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       locale: props.locale || 'NL-nl',
       hour: props.hour || '2-digit',
       minute: props.minute || '2-digit'
-    }
+    };
   }
   
   render() {
     let s = this.props.date ?
       this.props.date.toLocaleTimeString(
         this.state.locale,
-        { hour: this.state.hour, minute: this.state.minute }
-      ) : ''
+        { hour: this.state.hour, minute: this.state.minute, hour12: false }
+      ) : '';
 
-    if ( s === '00:00' ) s = ''
+    if ( s === '00:00' ) s = '';
 
-    return <span>{s}</span>
+    return <span>{s}</span>;
   }
 }
