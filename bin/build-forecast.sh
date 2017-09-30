a3d294e6df15bf3f792a0eafdc1e8dc93732ca9a
Martin van Amersfoorth <mamersfo@gmail.com>
2017-09-30 14:16:07 +0200

    Uitslagen 30 september

--- bin/build-forecast.sh
+++ bin/build-forecast.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 lein build-forecast
-git add data/2017-18/forecast.json
-git commit -m "Updated weather forecast"
+
 
--- bin/build-home.sh
+++ bin/build-home.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-lein build-home
+lein build-forecast
 git add data/2017-18/forecast.json
 git commit -m "Updated weather forecast"
 git push
